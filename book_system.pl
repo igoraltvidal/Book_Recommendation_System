e41787ebdcf345eb033e66eb46431e5bdc390141
IgorAltVidal <igor.a.vidal@gmail.com>
2021-01-09 10:16:34 -0300

    Add files via upload
    v1

---
+++ book_system.pl
@@ -0,0 +1,343 @@
+/** Books example */
+/** EXECUTE: $ swipl "book_system.pl" */
+/** book(name, writer, gender, sub-gender, pages, year, ranking) */
+
+book('Harry Potter and the Half-Blood Prince (Harry Potter  #6)', 'J.K. Rowling/Mary GrandPré', 'Fiction', 'Fantasy', 652, 2006.0, 4.57).
+book('The Ultimate Hitchhiker Guide: Five Complete Novels and One Story (Hitchhiker Guide to the Galaxy 1-5)', 'Douglas Adams','Fiction', 'Science', 815, 2005.0, 4.38).
+book('Bill Bryson African Diary', 'Bill Bryson','History', 'Travel', 55, 2002.0, 3.44).
+book('Artesia: Adventures in the Known World', 'Mark Smylie', 'Fiction', 'Fantasy', 352, 2005.0, 4.13).
+book('The Heidi Chronicles', 'Wendy Wasserstein','Drama','Music', 81, 2002.0, 3.75).
+book('Tropic of Capricorn', 'Henry Miller','Fiction','Biography', 348, 1994.0, 3.78).
+book('The War', 'Marguerite Duras/Barbara Bray','History','War', 192, 1994.0, 3.85).
+book('Treasure Island', 'Robert Louis Stevenson','Fiction','Adventure', 245, 2005.0, 3.83).
+book('Americana', 'Don DeLillo','Fiction','Classic', 377, 1989.0, 3.43).
+book('Ghosts (The New York Trilogy 2)', 'Paul Auster','Fiction','Mistery', 96, 1987.0, 3.64).
+book('The Alchemist', 'Paulo Coelho/Alan R. Clarke/James Noel Smith','Fiction','Mistery', 192, 2006.0, 3.86).
+book('Memoirs of a Geisha', 'Arthur Golden','Fiction','Biography' , 503, 2005.0, 4.11).
+book('Last of the Amazons', 'Steven Pressfield','Fiction','War', 400, 2003.0, 3.76).
+book('Twelfth Night', 'William Shakespeare','Drama','Classic', 272, 2004.0, 3.98).
+book('The City of God', 'Augustine of Hippo/Thomas Merton/Marcus Dods','History','Classic', 905, 2000.0, 3.92).
+book('Into the Wild', 'Jon Krakauer','Drama','Romance', 207, 1997.0, 3.98).
+book('Born to Be Wild', 'Catherine Coulter','Fiction','Mistery', 354, 2006.0, 3.78).
+book('The Simple Art of Murder', 'Raymond Chandler','Drama','Mistery', 384, 1988.0, 4.16).
+book('Stephen Hawking Universe', 'John Boslough','Science','Science', 160, 1989.0, 4.02).
+book('A Home at the End of the World', 'Michael Cunningham','Fiction','Romance', 342, 2004.0, 3.91).
+book('23 Days in July: Inside the Tour de France and Lance Armstrong Record-Breaking Victory', 'John Wilcockson/Graham Watson','History','Travel', 344, 2005.0, 3.67).
+book('Truman', 'David McCullough','History','Biography', 1120, 1993.0, 4.12).
+book('Other Voices Other Rooms', 'Truman Capote','Fiction','Classic', 232, 1994.0, 3.8).
+book('Moby Dick', 'Herman Melville/William Hootkins','Fiction','Classic', 25, 2005.0, 3.5).
+book('The Time Machine', 'H.G. Wells/Cynthia Brantley Johnson/Benjamin Beard','Fiction','Adventure', 150, 2004.0, 3.89).
+book('On the Road', 'Jack Kerouac','Drama','Biography', 320, 2005.0, 3.63).
+book('Love Visions', 'Geoffrey Chaucer/Brian Stone','History','Fantasy', 272, 1983.0, 3.61).
+book('Robinson Crusoe', 'Daniel Defoe','Drama','Adventure' , 288, 2006.0, 3.67).
+book('The Sixteen Pleasures', 'Robert Hellenga','Fiction','Romance', 384, 1995.0, 3.59).
+book('The Story of Salt', 'Mark Kurlansky/S.D. Schindler','History','Science', 48, 2006.0, 4.08).
+book('The Nuclear Age', 'Tim OBrien','Drama','War', 320, 1996.0, 3.45).
+book('The Rescue', 'Nicholas Sparks','Fiction','Romance', 352, 2005.0, 4.11).
+book('Sherlock Holmes: The Unauthorized Biography', 'Nick Rennison','History','Biography' , 304, 2006.0, 4.26).
+book('The Sea', 'John Banville','Fiction','Romance', 195, 2005.0, 3.51).
+book('On Beauty', 'Zadie Smith','Fiction','Romance', 445, 2006.0, 3.73).
+book('Book of Haikus', 'Jack Kerouac','History','Classic' , 200, 2004.0, 3.91).
+book('Innocent Blood', 'P.D. James','Fiction','Mistery', 400, 2001.0, 3.68).
+book('The Da Vinci Code', 'Dan Brown','Fiction','Adventure' , 489, 2006.0, 3.84).
+book('About a Boy', 'Nick Hornby','Fiction','Romance', 278, 2002.0, 3.8).
+book('East of Eden', 'John Steinbeck','Fiction','Classic' , 601, 2002.0, 4.37).
+book('The Far Side of Evil', 'Sylvia Engdahl','Fiction','Fantasy', 324, 2005.0, 3.98).
+book('Everything Is Illuminated', 'Jonathan Safran Foer','Fiction','Biography', 276, 2005.0, 3.9).
+book('The Great Gatsby', 'F. Scott Fitzgerald/Alexander Scourby','Drama','Romance', 4, 2002.0, 3.91).
+book('Merry Christmas  Stinky Face', 'Lisa McCourt/Cyd Moore','Fiction','Adventure', 32, 2004.0, 3.97).
+book('Night Over Water', 'Ken Follett','Fiction','Fantasy', 448, 2004.0, 3.82).
+book('Island', 'Aldous Huxley','Fiction','Classic', 354, 2002.0, 3.83).
+book('Drowning Ruth', 'Christina Schwarz','Drama','Mistery', 368, 2001.0, 3.74).
+book('Open House', 'Elizabeth Berg','Fiction','Romance' , 272, 2001.0, 3.7).
+book('The Bluest Eye', 'Toni Morrison','History','Classic' , 216, 2000.0, 4.03).
+book('An Ideal Husband', 'Oscar Wilde','Drama','Classic', 78, 2001.0, 4.04).
+book('Orientalism', 'Edward W. Said','History','Classic' , 396, 2003.0, 4.08).
+book('Brave New World', 'Aldous Huxley','Fiction','Science', 259, 2006.0, 3.99).
+book('The Diary of Anne Frank', 'Frances Goodrich/Albert Hackett/Wendy Kesselman','History','War', 70, 1998.0, 4.06).
+book('The Elephant War', 'Gillian Avery','History','War', 256, 2000.0, 3.67).
+book('If I Die in a Combat Zone', 'Tim OBrien','History','War', 208, 2003.0, 3.95).
+book('Magic Seeds', 'V.S. Naipaul','Fiction','Classic' , 280, 2005.0, 3.13).
+book('The Hobbit', 'J.R.R. Tolkien','Fiction','Adventure' , 277, 2007.0, 4.27).
+book('Love', 'Pablo Neruda','Drama','Romance', 43, 1995.0, 4.46).
+book('Creature', 'John Saul','Fiction','Romance', 416, 1997.0, 3.84).
+book('Hamlet', 'William Shakespeare','Drama','Romance' , 352, 2003.0, 4.02).
+book('Blade Runner', 'Philip K. Dick','Fiction','Adventure' , 216, 1987.0, 4.08).
+book('Crime and Punishment', 'Fyodor Dostoyevsky/David McDuff','Drama','Romance', 671, 2002.0, 4.21).
+book('One Hundred Years Of Solitude', 'Gabriel García Márquez','Drama','Romance', 416, 1995.0, 4.07).
+book('The Metamorphosis', 'Franz Kafka/Stanley Corngold','Fiction','Classic', 232, 1996.0, 3.81).
+book('Anne of Green Gables (Anne of Green Gables  #1)', 'L.M. Montgomery','Drama','Adventure', 320, 2003.0, 4.25).
+book('World War Z: An Oral History of the Zombie War', 'Max Brooks','Fiction','War', 342, 2006.0, 4.01).
+book('The Art of War', 'Sun Tzu/Ralph D. Sawyer','History','War' , 5, 2003.0, 3.97).
+book('A Farewell to Arms', 'Ernest Hemingway','Drama','War', 336, 1982.0, 3.8).
+book('National Geographic Traveler: Australia', 'Roff Smith','History','Travel' , 400, 1999.0, 3.8).
+book('The Shawshank Redemption', 'Mark Kermode/Rob White','Fiction','Classic', 96, 2003.0, 4.43).
+
+/** book(name, writer, gender, sub-gender, pages, year, ranking) */
+
+/** Functions */
+
+
+size(Y,O) 				:- book(Y,_,_,_,X,_,_), 20 < X, X < 90, O =='short'.
+size(Y,O)				:- book(Y,_,_,_,X,_,_), 91 < X, X < 400, O =='long'.
+size(Y,O)				:- book(Y,_,_,_,X,_,_), 401< X, O =='verylong'.
+
+ranking(Y,O)            :- book(Y,_,_,_,_,_,X), X > 4.0, O == 'top_ranked_book'.
+ranking(Y,O)            :- book(Y,_,_,_,_,_,X), X < 4.0, O == 'normal_ranked_book'.
+
+year(Y,O)			    :- book(Y,_,_,_,_,X,_), X < 2000, O =='old'.
+year(Y,O)				:- book(Y,_,_,_,_,X,_), 2001 < X, X < 2010, O =='normal'.
+year(Y,O)				:- book(Y,_,_,_,_,X,_), 2011 < X, O =='new'.
+
+/** REASONING FUNCTIONS */ 
+/** check(Y,genre,subgenre,size,year,rank) **/
+
+check(Y,O1,O2,O3,O4,O5)	:- test(Y,O1,O2,O3,O4,O5).
+check(Y,O1,O2,O3,O4,O5)	:- test(Y,O1,O2,O3,O4).
+check(Y,O1,O2,O3,O4,O5)	:- test(Y,O3,O4).
+check(Y,O1,O2,O3,O4,O5)	:- test(Y,O1,O2).
+check(Y,O1,O2,O3,O4,O5).
+
+test(Y,O1,O2,O3,O4,O5) 	    :- book(Y,_,O1,_,_,_,_), 
+                               book(Y,_,_,O2,_,_,_), 
+                               size(Y,O3), 
+                               year(Y,O4),
+                               ranking(Y,O5), !.
+test(Y,O1,O2,O3,O4) 	    :- book(Y,_,O1,_,_,_,_), 
+                               book(Y,_,_,O2,_,_,_), 
+                               size(Y,O3), 
+                               year(Y,O4), !.
+test(Y,O3,O4) 	            :- size(Y,O3), 
+                               year(Y,O4),!.
+test(Y,O1,O2) 	            :- book(Y,_,O1,_,_,_,_), 
+                               book(Y,_,_,O2,_,_,_), !.
+
+/** EXPERT SYSTEM */
+
+start:-	
+
+                write('Expert System - Book Recommender'),nl,
+                write('Please answer questions below'),nl,
+               	write('It helps to find books for your preference'),nl,nl,
+
+/** QUESTION - NAME */
+
+    write('   What is your name?'),nl,
+    read(Name),nl,
+
+/** QUESTION - GENDER */
+
+    write('   Which gender do you identify yourself?'),nl,
+    write('1. male'),nl,
+    write('2. female'),nl,
+    write('3. other'),nl,
+    read(PersonGender),nl,
+
+/** QUESTION - TIME */
+
+    write('   How much time do you have?'),nl,
+    write('1. Little'),nl,
+    write('2. Medium'),nl,
+    write('3. Many'),nl,
+    read(PersonalTime),nl,
+        (PersonalTime == 'little' -> BookSize = 'short'; 
+        PersonalTime == 'medium' -> BookSize = 'long';
+        PersonalTime == 'many' -> BookSize = 'verylong'),
+
+/** QUESTION - RANKING */
+
+    write('   Do you prefer top ranked books?'),nl,
+    write('1. top_ranked_book'),nl,
+    write('2. normal_ranked_book'),nl,
+    read(RankedBooksPref),nl,
+
+/** QUESTION - OLD BOOKS */
+
+    write('   How old you prefer the books?'),nl,
+    write('1. new'),nl,
+    write('2. normal'),nl,
+    write('3. old'),nl,
+    read(BooksAge),nl,
+
+/** QUESTION - AUTHOR */
+
+    /**write('   Do you prefer some author?'),nl,
+    read(AuthorPreference),nl,*/
+
+/** QUESTION - KIND OF BOOK */
+
+    write('   What kind of book would you like to read?'),nl,
+    write('1. scientific_or_technology'),nl,
+    write('2. narrative'),nl,
+    read(KindOfBook),nl,
+
+/** GENRE QUESTIONS **/
+    /** QUESTION - SCIENTIFIC BOOKS **/
+    (KindOfBook == 'scientific_or_technology' ->
+        write('   Are you searching for scientific books?'),nl,
+        write('1. no'),nl,
+        write('2. yes'),nl,
+        read(SearchScientificBooks),nl,
+        (
+            SearchScientificBooks == 'no' -> Genre = 'Fiction';
+            SearchScientificBooks == 'yes' -> Genre = 'Science', Subgenre = 'Science'
+        )
+    ;write('')),
+
+    /** QUESTION - SCIENTIFIC BOOKS **/
+    (KindOfBook == 'narrative' ->
+        write('   Do you enjoy learning about the cultures of different civizaltion?'),nl,
+        write('1. no'),nl,
+        write('2. yes'),nl,
+        read(LearningAboutDiffCivi),nl,
+        (
+            LearningAboutDiffCivi == 'no' -> Genre = 'Drama';
+            LearningAboutDiffCivi == 'yes' -> Genre = 'History'
+        )
+    ;write('')),
+
+    /** QUESTION - SUPERNATURAL STORIES **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (SearchScientificBooks == 'no' ->
+            write('   Do you like supernatural stories?'),nl,
+            write('1. no'),nl,
+            write('2. yes'),nl,
+            read(SupernaturalStories),nl
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - MOOD **/
+    (KindOfBook == 'narrative' ->
+        (LearningAboutDiffCivi == 'no' ->
+            write('   What is your actual mood?'),nl,
+            write('1. calm'),nl,
+            write('2. normal'),nl,
+            write('3. whimsical'),nl,
+            read(ActualMood),nl,
+            (
+                ActualMood == 'calm' -> (PersonGender == 'male' -> Subgenre = 'Music';
+                                         PersonGender == 'female' -> Subgenre = 'Romance';
+                                         PersonGender == 'other' -> Subgenre = 'Romance');
+                ActualMood == 'normal' -> Subgenre = 'Classic'; Subgenre = 'Biography'; Subgenre = 'War';
+                ActualMood == 'whimsical' -> Subgenre = 'Mistery'; Subgenre = 'Adventure'
+
+            )
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - FICTIONAL STORIES **/
+    (KindOfBook == 'narrative' ->
+        (LearningAboutDiffCivi == 'yes' ->
+            write('   What about the histories of a fictional universe?'),nl,
+            write('1. not_interested'),nl,
+            write('2. i_like_it'),nl,
+            read(FictionalUniverse),nl
+        ;write(''))
+    ;write('')),
+
+/** SUB GENRE QUESTIONS **/
+    /** QUESTION - FICTIONAL STORIES **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (SearchScientificBooks == 'no' ->
+            (SupernaturalStories == 'no' ->
+                write('   What do you prefer?'),nl,
+                write('1. romantic_story'),nl,
+                write('2. superhero_life_story'),nl,
+                write('3. i_dont_know'),nl,
+                read(FictionWhatDoYouPrefer),nl,
+                (
+                    FictionWhatDoYouPrefer == 'romantic_story' -> Subgenre = 'Romance';
+                    FictionWhatDoYouPrefer == 'superhero_life_story' -> Subgenre = 'Biography';
+                    FictionWhatDoYouPrefer == 'i_dont_know' -> Subgenre = 'Classic'
+                )
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - EPIC BATTLES **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (SearchScientificBooks == 'no' ->
+            (SupernaturalStories == 'yes' ->
+                write('   Do you enjoy reading histories with epic battles?'),nl,
+                write('1. no'),nl,
+                write('2. yes'),nl,
+                read(HistEpicBattles),nl,
+                (
+                    HistEpicBattles == 'no' -> Subgenre = 'Mistery'; Subgenre = 'Fantasy'
+                )
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - ADVENTURE OR WAR **/
+    (KindOfBook == 'narrative' ->
+        (SearchScientificBooks == 'no' ->
+            (SupernaturalStories == 'yes' ->
+                (HistEpicBattles == 'yes' ->
+                    write('   Please choose one of the options bellow'),nl,
+                    write('1. adventure'),nl,
+                    write('2. war'),nl,
+                    read(AdventureOrWar),nl,
+                    (
+                        AdventureOrWar == 'adventure' -> Subgenre = 'Adventure';
+                        AdventureOrWar == 'war' -> Subgenre = 'War'
+                    )
+                ;write(''))
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - FAMOUS HIST FIGURES **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (LearningAboutDiffCivi == 'yes' ->
+            (FictionalUniverse == 'not_interested' ->
+                write('   Histories of famous historical figures?'),nl,
+                write('1. no'),nl,
+                write('2. yes'),nl,
+                read(FamousHistFigures),nl,
+                (
+                    FamousHistFigures == 'no' -> Subgenre = 'Classic';
+                    FamousHistFigures == 'yes' -> Subgenre = 'Biography'
+                )
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - MULTIPLE NATIONS **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (LearningAboutDiffCivi == 'yes' ->
+            (FictionalUniverse == 'i_like_it' ->
+                write('   Histories which involves multiples nations?'),nl,
+                write('1. no'),nl,
+                write('2. yes'),nl,
+                read(MultipleNations),nl,
+                (
+                    MultipleNations == 'yes' -> Subgenre = 'War'; Subgenre = 'Adventure'
+                )
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+    /** QUESTION - NATURAL OR SUPERNATURAL **/
+    (KindOfBook == 'scientific_or_technology' ->
+        (LearningAboutDiffCivi == 'yes' ->
+            (FictionalUniverse == 'i_like_it' ->
+                (MultipleNations == 'no' ->
+                    write('   Natural or supernatural?'),nl,
+                    write('1. natural'),nl,
+                    write('2. supernatural'),nl,
+                    read(NaturalOrSupernatural),nl,
+                    (
+                        NaturalOrSupernatural == 'natural' -> Subgenre = 'Science';
+                        NaturalOrSupernatural == 'supernatural' -> Subgenre = 'Fantasy'
+                    )
+                ;write(''))
+            ;write(''))
+        ;write(''))
+    ;write('')),
+
+/** REASONING */
+    /** check(Y,genre,subgenre,size,year,rank) **/
+    check(Y, Genre, Subgenre, BookSize, BooksAge, RankedBooksPref),
+    write('You should read this book:'),nl,
+    write(Y),nl.
